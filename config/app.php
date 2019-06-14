bedf0be64cef904c77925c628672867aa496ca0f
Shahzaiibimran <Shahzaiibimran@gmail.com>
2019-06-14 18:04:12 +0500

    16-14-06-19

--- config/app.php
+++ config/app.php
@@ -56,8 +56,6 @@ return [
 
     'asset_url' => env('ASSET_URL', null),
 
-    'frontend_url' => 'shopker.pk/',
-
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
